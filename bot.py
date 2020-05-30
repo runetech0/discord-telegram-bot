e14f024c050ef0810f6c769dff29f81ca402ecc2
Rehman Ali <rehmanali.9442289@gmail.com>
2020-05-30 11:08:46 +0500

    Implement socks5 proxy

--- bot.py
+++ bot.py
@@ -4,15 +4,16 @@ Python version used: Python3
 
 NOTE: Please don't mess with code if you don't understand what you are doing.
 '''
-
-
+import socks
 import discord
-import asyncio
+from discord import errors
 import requests
+import socket
 import conf as config
 
 bot = discord.Client()
 baseUrl = f"https://api.telegram.org/bot{config.TELEGRAM_BOT_TOKEN}"
+
 
 def replaceMentions(mentions, msg, channel):
     if channel:
@@ -53,60 +54,72 @@ def matchChannel(channel, list):
     return found
 
 
+if config.PROXY:
+    socks.set_default_proxy(socks.SOCKS5, config.SOCKS5_SERVER, config.SOCKS5_PORT)
+    socket.socket = socks.socksocket
+    print(f"[+] Activated Proxy\n[+] Proxy Server: {config.SOCKS5_SERVER}:{config.SOCKS5_PORT}")
+
+
 @bot.event
 async def on_message(message):
-    try:
-        serverName = message.guild.name
-        serversList = config.serversList.keys()
-        channelName = message.channel.name
-        if serverName in serversList:
-            channelsList = config.serversList[serverName]
-            if matchChannel(channelName, channelsList):
-                # print(f"Matched channel: {channelName}")
-                if message.content:
-                    if message.mentions:
-                        message.content = replaceMentions(message.mentions, message.content, channel=False)
-                    if message.channel_mentions:
-                        message.content = replaceMentions(message.channel_mentions, message.content, channel=True)
-                    toSend = f"{message.guild}/{message.channel}/{message.author.name}: {message.content}"
+    serverName = message.guild.name
+    serversList = config.serversList.keys()
+    channelName = message.channel.name
+    print(f"Server: {serverName}, Channel: {channelName}")
+    if serverName in serversList:
+        channelsList = config.serversList[serverName]
+        if matchChannel(channelName, channelsList):
+            print(f"Matched channel: {channelName}")
+            if message.content:
+                if message.mentions:
+                    message.content = replaceMentions(message.mentions, message.content, channel=False)
+                if message.channel_mentions:
+                    message.content = replaceMentions(message.channel_mentions, message.content, channel=True)
+                toSend = f"{message.guild}/{message.channel}/{message.author.name}: {message.content}"
+                url = f"{baseUrl}/sendMessage?text={toSend}&chat_id={config.TELEGRAM_RECEIVER_CHAT_ID}"
+                requests.post(url)
+                print("Message sent!")
+                if message.attachments:
+                    attachmentUrl = message.attachments[0].url
+                    if isPhoto(attachmentUrl):
+                        url = f"{baseUrl}/sendPhoto?photo={attachmentUrl}&chat_id={config.TELEGRAM_RECEIVER_CHAT_ID}"
+                        requests.post(url)
+                    elif isVideo(attachmentUrl):
+                        url = f"{baseUrl}/sendVideo?video={attachmentUrl}&chat_id={config.TELEGRAM_RECEIVER_CHAT_ID}"
+                        requests.post(url)
+                    elif isDoc(attachmentUrl):
+                        url = f"{baseUrl}/sendDocument?document={attachmentUrl}&chat_id={config.TELEGRAM_RECEIVER_CHAT_ID}"
+                        requests.post(url)
+                
+            if message.embeds:
+                embed = message.embeds[0].to_dict()
+                print(embed)
+                if str(embed['type']) == "rich":
+                    if 'title' in embed.keys() and 'description' in embed.keys():
+                        toSend = f"{message.guild}/{message.channel}/{message.author.name}: {embed['title']}\n{embed['description']}"
+                    elif 'title' in embed.keys():
+                        toSend = f"{message.guild}/{message.channel}/{message.author.name}: {embed['title']}"
+                    elif 'description' in embed.keys():
+                        toSend = f"{message.guild}/{message.channel}/{message.author.name}: {embed['description']}"
                     url = f"{baseUrl}/sendMessage?text='{toSend}'&chat_id={config.TELEGRAM_RECEIVER_CHAT_ID}"
                     requests.post(url)
-                    if message.attachments:
-                        attachmentUrl = message.attachments[0].url
-                        if isPhoto(attachmentUrl):
-                            url = f"{baseUrl}/sendPhoto?photo={attachmentUrl}&chat_id={config.TELEGRAM_RECEIVER_CHAT_ID}"
-                            requests.post(url)
-                        elif isVideo(attachmentUrl):
-                            url = f"{baseUrl}/sendVideo?video={attachmentUrl}&chat_id={config.TELEGRAM_RECEIVER_CHAT_ID}"
-                            requests.post(url)
-                        elif isDoc(attachmentUrl):
-                            url = f"{baseUrl}/sendDocument?document={attachmentUrl}&chat_id={config.TELEGRAM_RECEIVER_CHAT_ID}"
-                            requests.post(url)
-                    
-                if message.embeds:
-                    embed = message.embeds[0].to_dict()
-                    print(embed)
-                    if str(embed['type']) == "rich":
-                        if 'title' in embed.keys() and 'description' in embed.keys():
-                            toSend = f"{message.guild}/{message.channel}/{message.author.name}: {embed['title']}\n{embed['description']}"
-                        elif 'title' in embed.keys():
-                            toSend = f"{message.guild}/{message.channel}/{message.author.name}: {embed['title']}"
-                        elif 'description' in embed.keys():
-                            toSend = f"{message.guild}/{message.channel}/{message.author.name}: {embed['description']}"
-                        url = f"{baseUrl}/sendMessage?text='{toSend}'&chat_id={config.TELEGRAM_RECEIVER_CHAT_ID}"
-                        requests.post(url)
-                        # print(embed)
-                    elif str(embed['type']) == "link":
-                        toSend = f"{embed['title']}\n{embed['description']}\n{embed['url']}"
-                        url = f"{baseUrl}/sendMessage?text='{toSend}'&chat_id={config.TELEGRAM_RECEIVER_CHAT_ID}"
-                        requests.post(url)
-    except RuntimeError:
-        print("\n\nPlease Wait ...\nShutting down the bot ... \n")
+                    # print(embed)
+                elif str(embed['type']) == "link":
+                    toSend = f"{embed['title']}\n{embed['description']}\n{embed['url']}"
+                    url = f"{baseUrl}/sendMessage?text='{toSend}'&chat_id={config.TELEGRAM_RECEIVER_CHAT_ID}"
+                    requests.post(url)
 
 
 #Run the bot using the user token
 try:
     bot.run(config.USER_DISCORD_TOKEN, bot=False)
+    #print(f"\n-------------------------------\n[+] Bot is up!\n-------------------------------\n")
 except RuntimeError:
     print("\n\nPlease Wait ...\nShutting down the bot ... \n")
+    quit()
+except errors.HTTPException:
+    print("Invalid discord token or network down!")
+    quit()
+except errors.LoginFailure:
+    print("Login failed to discord. May be bad token or network down!")
     quit()
--- conf.py
+++ conf.py
@@ -1,7 +1,7 @@
 
 # The Discord user token. It is a bit hard to obtain. You can follow the online tutorial here to
 # obtain your user token for discord. Tutorial: https://www.youtube.com/watch?v=tI1lzqzLQCs&feature=emb_logo
-USER_DISCORD_TOKEN = "discord_user_token_here"
+USER_DISCORD_TOKEN = 'NzE0MDcxMjcxNzcxNzM0MDQ2.XspUzw.9umL2DgXFRL7x54mFxnqvo9E7dw'  #"discord_user_token_here"
 
 # Go to your telegram account.
 # In the left upper corner in the search box search fot BotFather
@@ -11,7 +11,7 @@ USER_DISCORD_TOKEN = "discord_user_token_here"
 # Now after the username is set BotFather will give you your bot token paste it here below.
 # Altenatively, Follow this (https://core.telegram.org/bots#6-botfather) to create your bot on telegram. It's super easy.
 
-TELEGRAM_BOT_TOKEN =  "bot_token_here"
+TELEGRAM_BOT_TOKEN = "1105725049:AAGtuXqby-hog2INCN_v_nULv_bCej9lpNg"  #"bot_token_here"
 
 
 
@@ -22,7 +22,7 @@ TELEGRAM_BOT_TOKEN =  "bot_token_here"
 # Find the channel chat id under the chat. Copy and paste here.
 # Alternatively you can use get_id.py script from telegram-msg-forwarder folder. But for that to work you will have to setup the forwarder first.
 
-TELEGRAM_RECEIVER_CHAT_ID = "telegram_channel_chat_id_here"
+TELEGRAM_RECEIVER_CHAT_ID = -1001499013531      #"telegram_channel_chat_id_here"
 
 
 
@@ -32,5 +32,15 @@ TELEGRAM_RECEIVER_CHAT_ID = "telegram_channel_chat_id_here"
 
 serversList = {
     "Server-Name": ["Channel1", "Channel2", "Channel3", "Channel4"],
-    "Python": ["help-sodium", "help-zinc", "help-carbon", "help-magincium"]
+    "Python": ["discord․py", "help-sodium", "help-zinc", "help-carbon", "help-magincium"],
+    "r/Jailbreak": ["general", "checkra1n", "development"],
+    "EngineerMan": ["oof-topic"]
 }
+
+
+
+#SOCKS5 proxy settings
+# Set PROXY to False to disable the proxy settings
+PROXY = True
+SOCKS5_SERVER = '79.164.43.214'
+SOCKS5_PORT = 9050
